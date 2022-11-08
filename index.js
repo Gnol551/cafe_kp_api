8a1d18d19b83f247045a2d2b746bea69014a9488
trịnh bảo long <baolong551@gmail.com>
2022-11-08 22:36:08 +0700

    tich hop be

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ const app = express();
 const port = 5000;
 app.use(cors());
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 // Import Routes
 const homeRoute = require("./routes/home");
 const productsRoute = require("./routes/products");
+const authRoute = require("./routes/auth")
 
 // Routes
 app.use("/api/", homeRoute);
 app.use("/api/products", productsRoute);
+app.use('/api/auth', authRoute)
 
 app.listen(port, () =>
   console.log(`Hello world app listening on port ${port}!`)
---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.send({ id: 3, name: "Coffee" });
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  //res.send({ id: 3, name: "Coffee" });
+});
+
+router.put("/", (req, res) => {
+  res.send({ id: 3, name: "Coffee" });
+});
+
+router.delete("/", (req, res) => {
+  res.send({ id: 3, name: "Coffee" });
+});
+
+module.exports = router;
