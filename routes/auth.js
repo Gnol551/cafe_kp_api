435b325efddf93f0c9042238e4a2e5d4cfc1dca3
trịnh bảo long <baolong551@gmail.com>
2022-11-09 22:24:36 +0700

    basic login

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,31 @@
 const router = require("express").Router();
+const pool = require("../queries");
 
 router.get("/", (req, res) => {
   res.send({ id: 3, name: "Coffee" });
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  //res.send({ id: 3, name: "Coffee" });
+router.post("/", async (req, res) => {
+  try {
+    let result;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const users = await pool.query(
+      `SELECT * FROM users WHERE username = $1 and password = $2;`,
+      [username, password]
+    );
+
+    if (users.rows.length) {
+      result = true;
+    } else {
+      result = false;
+    }
+
+    res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.put("/", (req, res) => {
